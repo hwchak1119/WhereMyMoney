9768286f80b522399eb9e6976453ba381e6122a1
hwchak1119 <hwchak1119@gmail.com>
2023-08-03 15:02:18 +0800

    Expense screen UI

--- screens/Expenses.tsx
+++ screens/Expenses.tsx
@@ -1,6 +1,7 @@
-import { View, Text } from "react-native";
+import { View, Text, Button, TouchableOpacity } from "react-native";
 import ExpenseList from "../components/Expenses/ExpenseList";
 import { Recurrence } from "../types/constant";
+import { theme } from "../theme";
 
 const demoGroups = [
   {
@@ -52,6 +53,31 @@ const demoGroups = [
 export const Expenses = () => {
   return (
     <View>
+      <View
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          margin: theme.spacing.md,
+        }}
+      >
+        <Text style={{ color: theme.colors.info }}>Total </Text>
+        <TouchableOpacity>
+          <Text style={{ color: theme.colors.primary }}>This week</Text>
+        </TouchableOpacity>
+      </View>
+      <View
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "baseline",
+          gap: theme.spacing.sm,
+        }}
+      >
+        <Text style={{ color: theme.colors.info }}>HKD</Text>
+        <Text style={{ color: theme.colors.text, fontSize: 36 }}>195</Text>
+      </View>
       <ExpenseList groups={demoGroups} />
     </View>
   );
